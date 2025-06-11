7adb868178d9fc6ad63261758f31626736137506
Jorge Sola <jorge.sola@fersa.com>
2025-06-11 11:21:47 +0200

    nixpacks

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return f'Hola Buenas tardes - Current Time: {current_time}'
+    return f'Hola que tal - Current Time: {current_time}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
